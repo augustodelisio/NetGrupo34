0487c3fb3f7385ba9ed1a0af030c3f99a7283307
Augusto De Lisio <augustodelisio@gmail.com>
2020-06-07 13:14:43 -0300

    Ingreso tp1

---
+++ TP2/TP2L03/TP2/Business.Logic/UsuarioLogic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+
+namespace Business.Logic
+{
+    public class UsuarioLogic:BusinessLogic
+    {
+        public UsuarioLogic()
+        {
+            Data.Database.UsuarioAdapter UsuarioData 
+        }
+        public List<Usuario> GetAll()
+        {
+            return new List<Usuario>();//Reemplazar (punto 7)
+        }
+
+        public Usuario GetOne(int id)
+        {
+            return new Usuario();//Reemplazar (punto 8)
+        }
+
+        public void Delete(int id)
+        {
+
+        }
+        public void Save(Usuario usu)
+        {
+
+        }
+    }
+}

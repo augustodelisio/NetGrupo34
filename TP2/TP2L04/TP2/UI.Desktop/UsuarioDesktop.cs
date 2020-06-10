d1645f01aee9a073d2104ab76b4569834e487603
Augusto De Lisio <augustodelisio@gmail.com>
2020-06-10 19:59:21 -0300

    primera version lab4

---
+++ TP2/TP2L04/TP2/UI.Desktop/UsuarioDesktop.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business.Entities;
+using Business.Logic;
+using System.Text.RegularExpressions;
+
+namespace UI.Desktop
+{
+    public partial class UsuarioDesktop : ApplicationForm
+    {
+        public UsuarioDesktop()
+        {
+            InitializeComponent();
+        }
+        public UsuarioDesktop(ModoForm modo) : this()
+        {
+            InitializeComponent();
+        }
+        public UsuarioDesktop(int ID, ModoForm modo) : this()
+        {
+            UsuarioLogic ul = new UsuarioLogic();
+            this.UsuarioActual = ul.GetOne(ID);
+            this.MapearDeDatos();
+        }
+
+        private Usuario _UsuarioActual;
+        public Usuario UsuarioActual { set; get; }
+        public override void MapearDeDatos()
+        {
+            this.txtID.Text = this.UsuarioActual.ID.ToString();
+            this.chkHabilitado.Checked = this.UsuarioActual.Habilitado;
+            this.txtNombre.Text = this.UsuarioActual.Nombre;
+            this.txtApellido.Text = this.UsuarioActual.Apellido;
+            this.txtEmail.Text = this.UsuarioActual.EMail;
+            this.txtUsuario.Text = this.UsuarioActual.NombreUsuario;
+            this.txtClave.Text = this.UsuarioActual.Clave;
+            this.txtConfirmarClave.Text = this.UsuarioActual.Clave;
+            if (Modo == ModoForm.Alta || Modo == ModoForm.Modificacion) 
+            {
+                this.btnAceptar.Text = "Guardar";
+            }
+            else if (Modo == ModoForm.Baja)
+            {
+                this.btnAceptar.Text = "Eliminar";
+            }
+            else if (Modo == ModoForm.Consulta)
+            {
+                this.btnAceptar.Text = "Aceptar";
+            }
+        }
+        public override void MapearADatos()
+        {
+            if (Modo == ModoForm.Alta || Modo == ModoForm.Modificacion)
+            {
+                if (Modo == ModoForm.Modificacion)
+                {
+                    try { this.UsuarioActual.ID = int.Parse(this.txtID.Text); }
+                    catch {}
+                    UsuarioActual.State = BusinessEntity.States.Modified;
+
+                }
+                else
+                {
+                    Usuario usu = new Usuario();
+                    UsuarioActual = usu;
+                    UsuarioActual.State = BusinessEntity.States.New;
+                }
+                
+                this.UsuarioActual.Habilitado = this.chkHabilitado.Checked;
+                this.UsuarioActual.Nombre = this.txtNombre.Text;
+                this.UsuarioActual.Apellido = this.txtApellido.Text;
+                this.UsuarioActual.EMail = this.txtEmail.Text;
+                this.UsuarioActual.NombreUsuario = this.txtUsuario.Text;
+                this.UsuarioActual.Clave = this.txtClave.Text;
+            }
+            else if (Modo == ModoForm.Baja)
+            {
+                UsuarioActual.State = BusinessEntity.States.Deleted;
+            }
+            else if (Modo == ModoForm.Consulta)
+            {
+                UsuarioActual.State = BusinessEntity.States.Unmodified;
+            }
+            
+        }
+        public override void GuardarCambios()
+        {
+            this.MapearADatos();
+            UsuarioLogic usu = new UsuarioLogic();
+            usu.Save(UsuarioActual);
+        }
+        public override bool Validar()
+        {
+            bool correcto = true;
+            /*if (String.IsNullOrEmpty(this.txtNombre.Text) || String.IsNullOrEmpty(this.txtApellido.Text) || String.IsNullOrEmpty(this.txtEmail.Text) || String.IsNullOrEmpty(this.txtClave.Text) || String.IsNullOrEmpty(this.txtUsuario.Text))
+                correcto = false;*/
+            if (this.txtClave.Text != this.txtConfirmarClave.Text)
+                correcto = false;
+            /*if (ChequeaMail(this.txtEmail.Text) == false)
+                correcto = false;*/
+
+            if (correcto == false)
+                this.Notificar("Error, datos mal ingresados, verifique la contraseña y el mail", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            return correcto;
+        }
+
+        public bool ChequeaMail(string email)
+        {
+            string expresion;
+            expresion = "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
+            if (Regex.IsMatch(email, expresion))
+            {
+                if (Regex.Replace(email, expresion, String.Empty).Length == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            bool res = this.Validar();
+            if (res)
+            {
+                this.GuardarCambios();
+                this.Close();
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+    
+}
